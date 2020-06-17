7dabe539485a9629f2538b52ab1d54d6dd0249c4
cbson <cbson@mtcom.co.kr>
2020-06-17 11:26:29 +0900

    first commit

---
+++ Example/Pods/Target Support Files/Pods-TATLib_Tests/Pods-TATLib_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TATLib_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_TATLib_Tests
+@end
---
+++ Example/Pods/Target Support Files/TATLib/TATLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TATLib : NSObject
+@end
+@implementation PodsDummy_TATLib
+@end
